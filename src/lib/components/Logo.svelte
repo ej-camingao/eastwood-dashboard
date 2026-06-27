33826454947725eb10baa3b51fd82774080e7f14
ej-camingao <ethancamingao@gmail.com>
2026-06-27 13:45:20 +0800

    feat: change the html logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -16,7 +16,7 @@
 
 <div class="flex justify-center {className}">
 	<img
-		src="/elevate-full-logo.png"
+		src="/elevate-logo.png"
 		alt="Elevate Eastwood"
 		class="{sizeClasses[size]} w-auto object-contain"
 	/>
